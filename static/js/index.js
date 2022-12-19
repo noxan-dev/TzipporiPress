3cbde4bb2388d04a84b0134b155f635dfdd986fc
Chaim Malek <chaimmalek@gmail.com>
2022-12-19 12:38:34 -0500

    navbar transition, more styling, svg changes

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,4 @@
+window.addEventListener('scroll', function () {
 
-
+    document.querySelector('.nav-container').classList.toggle('scrolled', window.scrollY > document.querySelector('.nav-container').offsetHeight);
+});
