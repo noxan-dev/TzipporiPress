d4a303b5afe58c71ab1d2f8023d6d2c1bb430e9d
Chaim Malek <chaimmalek@gmail.com>
2023-02-05 14:05:12 -0500

    start redesign

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,11 @@
 window.addEventListener('scroll', function () {
     document.querySelector('.nav-container').classList.toggle('scrolled', window.scrollY > document.querySelector('.nav-container').offsetHeight);
 });
+
+
+let animation = anime({
+        targets: '#services .item-1',
+        left: '20px',
+        easing: 'easeInOutQuad',
+        duration: 1000,
+    });
