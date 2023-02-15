17e8206c8b2a36ef3a1a9be4f6233c93e1c713fa
Chaim Malek <chaimmalek@gmail.com>
2023-02-15 10:51:59 -0500

    gradiant blend, remove spacing need to fix medium layout, Nav moved

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,25 @@
-window.addEventListener('scroll', function () {
-    document.querySelector('.nav-container').classList.toggle('scrolled', window.scrollY > document.querySelector('.nav-container').offsetHeight);
-});
+// window.addEventListener('scroll', function () {
+//     document.querySelector('.nav-container').classList.toggle('scrolled', window.scrollY > document.querySelector('.nav-container').offsetHeight);
+// });
+//
+//
+// let animation = anime({
+//         targets: '#services .item-1',
+//         left: '20px',
+//         easing: 'easeInOutQuad',
+//         duration: 1000,
+//     });
 
+// const input = document.getElementsByClassName('input');
 
-let animation = anime({
-        targets: '#services .item-1',
-        left: '20px',
-        easing: 'easeInOutQuad',
-        duration: 1000,
-    });
+// for (let i = 0; i < input.length; i++) {
+//     input[i].addEventListener('invalid', function (event) {
+//         if (event.target.validity.valueMissing) {
+//             event.target.setCustomValidity('Add a name before submitting for a quote.');
+//         }
+//     })
+// }
+//
+// input.addEventListener('change', function (event) {
+//     event.target.setCustomValidity('');
+// })
