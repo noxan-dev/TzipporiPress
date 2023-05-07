e9bd915ba560463f80b81b507361b078fbbe66cc
Chaim Malek <chaimmalek@gmail.com>
2023-05-07 15:07:53 -0400

    things work don't touch. Moved around styles. removed unused styles.

--- static/js/index.js
+++ static/js/index.js
@@ -18,6 +18,14 @@ let carouselinner = $('.carousel-inner');
 $.getJSON('https://noxan-dev.github.io/TzipporiPress/testimonials.json', function (data) {
     var i= 0;
     $.each(data, function (key, val) {
+        carouselinner.append(
+            `<div class="carousel-item ${key == 0 ? 'active' : ''}">
+                <p><span class="quote">“</span>${val.text}</p>
+                <h4>${val.author}</h4>
+                <span>${val.title}</span>
+            </div>`
+        );
+
         carouselindicators.append(
             `<button type="button" 
                     data-bs-target="#carouselTestimonials" 
@@ -25,15 +33,8 @@ $.getJSON('https://noxan-dev.github.io/TzipporiPress/testimonials.json', functio
                     aria-label="Slide ${i}"
                     class="slide-${i} ${key == 0 ? 'active' : ''}"
                     ${key == 0 ? 'aria-current="true"': ''} 
-                    style="background-image: url('${val.image}')"></button>`
+                    style="background-image: url('${val.image}')"></button>`,
         );
         i++;
-        carouselinner.append(
-            `<div class="carousel-item ${key == 0 ? 'active' : ''}">
-                <p><span class="quote">“</span><br>${val.text}</p>
-                <h4>${val.author}</h4>
-                <span>${val.title}</span>
-            </div>`
-        );
     });
 });
