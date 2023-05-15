5d783824f19f3b9ed67bbbc5665d76d29d293425
Chaim Malek <chaimmalek@gmail.com>
2023-05-15 13:27:14 -0400

    more work to the testimonials. Fixed up Footer

--- static/js/index.js
+++ static/js/index.js
@@ -12,21 +12,19 @@ $(document).ready(function () {
     });
 });
 
+
 $(document).ready(function () {
     let carouselIndicators = $('.carousel-indicators');
     let carouselInner = $('.carousel-inner');
+    let carouselAuthorInfo = $('.carousel-author-info');
 
-    $.getJSON('https://noxan-dev.github.io/TzipporiPress/testimonials.json', function (data) {
+    $.getJSON('./testimonials.json', function (data) {
         let i = 0;
         $.each(data, function (key, val) {
             carouselInner.append(
                 `<div class="carousel-item ${i === 0 ? 'active' : ''}">
-                    <p><span class="quote">“</span>${val.text}</p>
-                    <div class="author-info">
-                      <h4>${val.author}</h4>
-                      <span>${val.title}</span>
-                    </div>
-                  </div>`
+                     <p><span class="quote">“</span>${val.content}</p>
+                </div>`
             );
 
             carouselIndicators.append(
@@ -34,12 +32,31 @@ $(document).ready(function () {
                     data-bs-target="#carouselTestimonials" 
                     data-bs-slide-to="${i}" 
                     aria-label="Slide ${i}"
-                    class="slide-${i} ${i === 0 ? 'active' : ''}"
+                    class="rounded-circle slide-${i} ${i === 0 ? 'active' : ''}"
                     ${i === 0 ? 'aria-current="true"' : ''} 
                     style="background-image: url('${val.image}')"></button>`
             );
+            carouselAuthorInfo.append(
+                `<div class="carousel-item author-info ${i === 0 ? 'active' : ''}">
+                    <h4>${val.author}</h4>
+                    <span>${val.title}</span>
+                </div>`
+            );
             i++;
         });
+
+        let numIndicators = carouselIndicators.children().length;
+        const maxVisibleIndicators = 3;
+        if (numIndicators > maxVisibleIndicators) {
+            carouselIndicators.children().slice(maxVisibleIndicators).hide();
+        }
+
         $('.carousel').carousel();
+
+    });
+
+    $('.carousel').on('slid.bs.carousel', function (e) {
+        carouselAuthorInfo.find('.carousel-item').removeClass('active');
+        carouselAuthorInfo.find(`.carousel-item:nth-child(${e.to + 1})`).addClass('active');
     });
 });
