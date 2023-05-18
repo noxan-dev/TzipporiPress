a6c37c92cd50ac46eaf7251e1bbd95e139ebbaa9
Chaim Malek <chaimmalek@gmail.com>
2023-05-18 16:09:03 -0400

    hiding more than 3 indicators, enabled. Starting with a testimonial.

--- static/js/index.js
+++ static/js/index.js
@@ -12,18 +12,18 @@ $(document).ready(function () {
     });
 });
 
-
 $(document).ready(function () {
     let carouselIndicators = $('.carousel-indicators');
     let carouselInner = $('.carousel-inner');
     let carouselAuthorInfo = $('.carousel-author-info');
+    const IndicatorsInterval = 5000;
 
     // ./testimonials.json
     $.getJSON('https://noxan-dev.github.io/TzipporiPress/testimonials.json', function (data) {
         let i = 0;
         $.each(data, function (key, val) {
             carouselInner.append(
-                `<div class="carousel-item">
+                `<div class="carousel-item ${i === 1 ? 'active' : ''}">
                  <p><span class="quote">“</span>${val.content}</p>
             </div>`
             );
@@ -33,20 +33,36 @@ $(document).ready(function () {
                 data-bs-target="#carouselTestimonials" 
                 data-bs-slide-to="${i}" 
                 aria-label="Slide ${i}"
-                class="rounded-circle slide-${i}"
+                class="rounded-circle slide-${i} ${i === 1 ? 'active' : ''}"
                 style="background-image: url('${val.image}')"
                 data-order="${i}"></button>`
             );
 
             carouselAuthorInfo.append(
-                `<div class="carousel-item author-info">
+                `<div class="carousel-item author-info ${i === 1 ? 'active' : ''}">
                 <h4>${val.author}</h4>
                 <span>${val.title}</span>
             </div>`
             );
             i++;
         });
+        hideExtraIndicators();
     });
+
+    function hideExtraIndicators() {
+        let indicators = carouselIndicators.children();
+
+        indicators.each(function () {
+            let order = parseInt($(this).attr('data-order'));
+
+            // Show only indicators with data-order values of 0, 1, and 2
+            if (order >= 0 && order <= 2) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
 
     function moveIndicators(targetIndex) {
         let indicators = carouselIndicators.children();
@@ -66,13 +82,6 @@ $(document).ready(function () {
             }
             $(this).attr('data-order', order);
             $(this).css('order', order);
-
-            // Show only indicators with data-order values of 0, 1, and 2
-            if (order >= 0 && order <= 2) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
         });
 
         carouselIndicators.find('.active').removeClass('active');
@@ -86,25 +95,29 @@ $(document).ready(function () {
         // Update the active author info
         carouselAuthorInfo.find('.carousel-item').removeClass('active');
         carouselAuthorInfo.find(`.carousel-item:nth-child(${newActiveIndex + 1})`).addClass('active');
+
+        hideExtraIndicators();
     }
 
-    let moveIndicatorsInterval = setInterval(moveIndicators, 5000);
+    let moveIndicatorsInterval = setInterval(moveIndicators, IndicatorsInterval);
     const carousel = $('.carousel');
 
+    // Stop the carousel from moving when the mouse is over it
     carousel.hover(function () {
         clearInterval(moveIndicatorsInterval);
     }, function () {
-        moveIndicatorsInterval = setInterval(moveIndicators, 5000);
+        moveIndicatorsInterval = setInterval(moveIndicators, IndicatorsInterval);
     });
 
-
+    // Move the indicators when the user clicks on one
     carouselIndicators.on('click', 'button', function () {
         let targetIndex = $(this).data('order');
         let activeIndex = carouselIndicators.find('.active').index();
 
         if (targetIndex !== activeIndex) {
-            clearInterval(moveIndicators);
+            clearInterval(moveIndicatorsInterval);
             moveIndicators(targetIndex);
+            moveIndicatorsInterval = setInterval(moveIndicators, IndicatorsInterval);
         }
     });
 });
