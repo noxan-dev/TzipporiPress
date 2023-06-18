1c2cbf554603dc8a26788e27130f23d54562595b
Chaim Malek <chaimmalek@gmail.com>
2023-06-18 14:15:09 -0400

    More Nav changes. Footer clean up

--- static/js/index.js
+++ static/js/index.js
@@ -124,13 +124,13 @@ $(document).ready(function () {
     });
 });
 
-// Get the .nav-items element
-var navItems = document.querySelector('.nav-items');
-
-// Set the height of the .nav-items element to the height of the window
-navItems.style.height = window.innerHeight + 'px';
-
-// Update the height of the .nav-items element when the window is resized
-window.addEventListener('resize', function() {
-    navItems.style.height = window.innerHeight + 'px';
-});
+// // Get the .nav-items element
+// var navItems = document.querySelector('.nav-items');
+//
+// // Set the height of the .nav-items element to the height of the viewport
+// navItems.style.height = window.innerHeight + 'px';
+//
+// // Update the height of the .nav-items element when the window is resized
+// window.addEventListener('resize', function() {
+//     navItems.style.height = window.innerHeight + 'px';
+// });
